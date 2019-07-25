35db9f4b70fb1d1742e26e2affd185db2ccfe362
Harsh Patel <harshaudi27@gmail.com>
2019-07-25 05:58:31 +0530

    Reformatted code

--- src/main/java/io/appbase/trial/Main.java
+++ src/main/java/io/appbase/trial/Main.java
@@ -1,4 +1,5 @@
 package io.appbase.trial;
+
 import java.io.IOException;
 
 import io.appbase.client.AppbaseClient;
@@ -6,42 +7,55 @@ import okhttp3.Response;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		AppbaseClient ac = new AppbaseClient("https://scalr.api.appbase.io", "shopify-flipkart-test", "xJC6pHyMz", "54fabdda-4f7d-43c9-9960-66ff45d8d4cf");
-		
-		String query = "{ \"query\":{ \"bool\":{ \"must\":{ \"bool\":{ \"should\":[ { \"multi_match\":{ \"query\": \"oth\" ," +
-				" \"fields\":[ \"title\", \"title.search\" ], \"operator\":\"and\" } }," +
-				" { \"multi_match\":{ \"query\": \"oth\" ,  \"fields\":[ \"title\", \"title.search\" ], \"type\":\"phrase_prefix\"," +
-				" \"operator\":\"and\" } } ], \"minimum_should_match\":\"1\" } } } } }";
+        AppbaseClient ac = new AppbaseClient("https://scalr.api.appbase.io", "shopify-flipkart-test", "xJC6pHyMz", "54fabdda-4f7d-43c9-9960-66ff45d8d4cf");
 
-		String analyticsQuery = "{ \"X-Search-Id\": \"2204483485796\", \"X-Search-Click\": \"true\", \"X-Search-ClickPosition\": \"2\", \"X-Search-Conversion\": \"true\" }";
+        String query =
+                "{ \"query\":{ " +
+                    "\"bool\":{ " +
+                        "\"must\":{ " +
+                            "\"bool\":{ " +
+                                "\"should\":[ " +
+                                    "{ \"multi_match\":{" +
+                                        " \"query\": \"oth\" ," +
+                                        " \"fields\":[ \"title\", \"title.search\" ]," +
+                                        " \"operator\":\"and\" } }," +
+                                    "{ \"multi_match\":{" +
+                                        " \"query\": \"oth\" ," +
+                                        " \"fields\":[ \"title\", \"title.search\" ]," +
+                                        " \"type\":\"phrase_prefix\"," +
+                                        " \"operator\":\"and\" } } ], " +
+                                "\"minimum_should_match\":\"1\" } } } } }";
 
-		// Making a search query
-		try {
-			Response response = ac.prepareSearch("products", query).execute();
-			String body = response.body().string();
-			String XSearchId = response.header("X-Search-Id");
-			System.out.println(body);
-			System.out.println("X-Search-Id " + XSearchId);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        String analyticsQuery = "{ " +
+                "\"X-Search-Id\": \"2204483485796\"," +
+                " \"X-Search-Click\": \"true\"," +
+                " \"X-Search-ClickPosition\": \"2\"," +
+                " \"X-Search-Conversion\": \"true\" " +
+                "}";
 
-		// Recording Analytics with default X-Search-Id
-		try {
-			String analytics = ac.prepareAnalytics(null).execute().body().string();
-			System.out.println(analytics);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        // Making a search query
+        try {
+            Response response = ac.prepareSearch("products", query).execute();
+            String body = response.body().string();
+            String XSearchId = response.header("X-Search-Id");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		// Recording Analytics with custom X-Search-Id
-		try {
-			String analytics = ac.prepareAnalytics(analyticsQuery).execute().body().string();
-			System.out.println(analytics);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        // Recording Analytics with default X-Search-Id
+        try {
+            String analytics = ac.prepareAnalytics(null).execute().body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Recording Analytics with custom X-Search-Id
+        try {
+            String analytics = ac.prepareAnalytics(analyticsQuery).execute().body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
